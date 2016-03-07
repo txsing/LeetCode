f27e499bc3ee01a89834ff133125c89214d286e7
tianxing <txsing@gmail.com>
2016-03-07 21:53:21 +0800

    add Rabin-Karp algorithm in Q28_Pattern String Matching strstr()

--- src/com/leetcode/algorithm/Q28_strstr.java
+++ src/com/leetcode/algorithm/Q28_strstr.java
@@ -1,12 +1,17 @@
 package com.leetcode.algorithm;
-class Q28_strstr{
-	
+
+class Q28_strstr {
+
 	public static void main(String[] args) {
 		String target = "babcbabcabcaabcabcabcacabc";
 		String pattern = "abcabcacab";
-		
-		System.out.println(strstr(target, pattern));
+
+		System.out.println(strstr1(target, pattern));
 	}
+
+	/*
+	 * KMP
+	 */
 	static int strstr(String t, String p) {
 		if (p.length() == 0)
 			return 0;
@@ -16,7 +21,7 @@ class Q28_strstr{
 		int tstart = 0;
 		int i = 0;
 		int j = 0;
-		while (tstart <= t.length()-p.length()) {
+		while (tstart <= t.length() - p.length()) {
 			for (i = tstart, j = 0; j < p.length(); j++, i++) {
 				if (p.charAt(j) == t.charAt(i)) {
 					continue;
@@ -50,4 +55,61 @@ class Q28_strstr{
 		}
 		return next;
 	}
+
+	/*
+	 * Rabin-Karp
+	 * expected matching complexity = O(M+N) when q >> M; preprocess complexity =o(M) 
+	 */
+	static int strstr1(String txt, String pat) {
+		int q = 101; //a prime number
+		int d = 95; //
+		int M = pat.length();
+		int N = txt.length();
+		int i, j;
+		int p = 0; // hash value for pattern
+		int t = 0; // hash value for txt
+		int h = 1;
+
+		// The value of h would be "pow(d, M-1)%q"
+		for (i = 0; i < M - 1; i++)
+			h = (h * d) % q;
+
+		// Calculate the hash value of pattern and first window of text
+		for (i = 0; i < M; i++) {
+			p = (d * p + pat.charAt(i)) % q;
+			t = (d * t + txt.charAt(i)) % q;
+		}
+
+		// Slide the pattern over text one by one
+		for (i = 0; i <= N - M; i++) {
+
+			// Check the hash values of current window of text and pattern
+			// If the hash values match then only check for characters on by one
+			if (p == t) {
+				/* Check for characters one by one */
+				for (j = 0; j < M; j++) {
+					if (txt.charAt(i + j) != pat.charAt(j))
+						break;
+				}
+				if (j == M) // if p == t and pat[0...M-1] = txt[i, i+1,
+							// ...i+M-1]
+				{
+					return i;
+				}
+			}
+
+			// Calulate hash value for next window of text: Remove leading
+			// digit,
+			// add trailing digit
+			if (i < N - M) {
+				t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + M)) % q;
+
+				// We might get negative value of t, converting it to positive
+				if (t < 0)
+					t = (t + q);
+			}
+		}
+		return -1;
+	}
+
 }
