51092b77b49ed68b3756f9ab5d2084776b340d97
tianxing <txsing@gmail.com>
2016-03-14 00:19:45 +0800

    update Q18

--- src/com/leetcode/algorithm/Q18_4Sum2.py
+++ src/com/leetcode/algorithm/Q18_4Sum2.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Q18_4Sum(object):
     def fourSum(self, nums, target):
         """
         :type nums: List[int]
@@ -6,34 +6,30 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         result = []
+        nums.sort()
         if(len(nums)<4):
             return result
         for i in range(0,len(nums)-3):
-            if(nums[i]>target):
-                break;
             #avoid duplication
             if(i > 0 and nums[i-1] == nums[i]):
                 continue;
-            intrm = threeSum(nums[i:], target-nums[i])
+            intrm = self.__threeSum(nums[i+1:], target-nums[i])
             if(len(intrm) != 0):
                 for lst in intrm:
-                    print(lst)
-                    result.append(nums[i]+lst);
+                    lst.insert(0,nums[i]);
+                    result.append(lst)
         return result
         
                 
-    def threeSum(nums, target):
+    def __threeSum(self,nums, target):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
         result =[]
-        nums.sort()
         if(len(nums)<=2):
             return result
         for i in range(0,len(nums)-2):
-            if(nums[i]>target):
-                break
             if(i>0 and nums[i-1] == nums[i]):
                 continue
             L, H = i+1, len(nums)-1 
