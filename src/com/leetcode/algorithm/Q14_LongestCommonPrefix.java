5a0c994d74e068247589c79ae0b8037d9782c541
tianxing <txsing@gmail.com>
2016-03-15 23:08:16 +0800

    add a new approach to solve Q14 Longest Common Prefix

--- src/com/leetcode/algorithm/Q14_LongestCommonPrefix.java
+++ src/com/leetcode/algorithm/Q14_LongestCommonPrefix.java
@@ -1,6 +1,11 @@
 package com.leetcode.algorithm;
 
 public class Q14_LongestCommonPrefix {
+	
+	/*
+	 * Make use of Java try-catch mechanism to handle out-of-bound problem 
+	 * Runtime: 3ms 
+	 */
     public String longestCommonPrefix(String[] strs) {
         String result ="";
         if(strs.length == 0){
@@ -20,5 +25,24 @@ public class Q14_LongestCommonPrefix {
             }
         }
         return result;
+    }
+    
+    /*
+     * Keep reducing strs[0] into target common prefix
+     * Runtime: 1ms
+     */
+    public String longestCommonPrefix2(String[] strs) {
+        if(strs == null || strs.length == 0){
+        	return "";
+        }
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0){
+                pre = pre.substring(0,pre.length()-1);
+            }
+            i++;
+        }
+        return pre;
     }
 }
