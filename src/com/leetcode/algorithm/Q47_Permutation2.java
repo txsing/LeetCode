a660406f0fd4f11554516beb703aad02efd0a82c
tianxing <txsing@gmail.com>
2016-03-18 22:01:29 +0800

    add a new way to solve Q47, reduce runtime to 8ms frn 65ms

--- src/com/leetcode/algorithm/Q47_Permutation2.java
+++ src/com/leetcode/algorithm/Q47_Permutation2.java
@@ -8,6 +8,10 @@ import java.util.*;
  *
  */
 public class Q47_Permutation2 {
+	List<Integer> path = new ArrayList<Integer>();
+	List<List<Integer>> results = new ArrayList<List<Integer>>();
+	
+	//runtime: 65ms
 	public List<List<Integer>> permuteUnique(int[] nums) {
 		List<List<Integer>> ans = new ArrayList<>();
 		if (nums.length == 0)
@@ -30,5 +34,52 @@ public class Q47_Permutation2 {
 			ans = new_ans;
 		}
 		return ans;
+	}
+	
+	/**
+	 * The basic concept behind this method is that, we build a tree with unique branches
+	 * e,g,. [1,1,2]: the tree need build is as follow
+	 * 	    1- 2
+	 *   1- 
+	 * 0-   2- 1
+	 *   2- 1- 1
+	 *
+	 *runtime: 8ms
+	 */
+	public List<List<Integer>> permuteUnique2(int[] nums){
+		boolean[] used = new boolean[nums.length]; 
+		Arrays.sort(nums);
+		find(nums, used);
+		return null;
+	}
+	
+	void find(int[] nums, boolean[] used){
+		//flag == false means, the next unused Integer should be added to the path
+		boolean flag = false;
+		for(int i = 0; i < nums.length; i++){
+			if(i > 0 && nums[i-1] != nums[i]){
+				flag = false;
+			}
+			if(used[i] == false && flag == false){
+				//the next unused Integer (if it equals to the last one)
+				//will result in duplication if added, so set flag to true
+				flag = true;
+				path.add(nums[i]);
+				used[i] = true;
+				if(path.size() < nums.length){
+					find(nums, used);
+				}else{
+					//reach the end of the tree
+					List<Integer> result = new ArrayList<>();
+					for(Integer integer : path){
+						result.add(integer);
+					}
+					results.add(result);
+				}
+				path.remove(path.size()-1);
+				used[i] = false;
+			}
+
+		}
 	}
 }
