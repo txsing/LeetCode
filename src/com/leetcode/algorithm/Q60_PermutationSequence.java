a67214a53f4dde58188b4977bd24038f3fff89de
tianxing <txsing@gmail.com>
2016-03-19 20:09:12 +0800

    add Q60 permutation sequence

---
+++ src/com/leetcode/algorithm/Q60_PermutationSequence.java
@@ -0,0 +1,22 @@
+package com.leetcode.algorithm;
+
+import java.util.ArrayList;
+
+public class Q60_PermutationSequence {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            num.add(i);
+        }
+        for (int i = 0, l = k - 1; i < n; i++) {
+            fact /= (n - i);
+            int index = (l / fact);
+            sb.append(num.remove(index));
+            l -= index * fact;
+        }
+        return sb.toString();
+    }
+}
