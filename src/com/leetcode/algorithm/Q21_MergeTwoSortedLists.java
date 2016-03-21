88a0cb1426105fde486e48860fa17b4cbb3f6224
tianxing <txsing@gmail.com>
2016-03-21 20:40:13 +0800

    Q21 add non-recursive method

--- src/com/leetcode/algorithm/Q21_MergeTwoSortedLists.java
+++ src/com/leetcode/algorithm/Q21_MergeTwoSortedLists.java
@@ -30,4 +30,22 @@ public class Q21_MergeTwoSortedLists {
 		}
 		return mergeHead;
 	}
+	
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+	    ListNode fakeHead = new ListNode(0);
+	    ListNode current = fakeHead;
+
+	    while (l1 != null || l2 != null) {
+	        if (l1 == null || (l2 != null && l1.val >= l2.val)) {
+	            current.next = l2;
+	            current = l2;
+	            l2 = l2.next;
+	        } else {
+	            current.next = l1;
+	            current = l1;
+	            l1 = l1.next;
+	        }
+	    }
+	    return fakeHead.next;
+	}
 }
