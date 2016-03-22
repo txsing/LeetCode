ab70afe68cf08798cc97fda84234707902b4d54b
tianxing <txsing@gmail.com>
2016-03-23 00:34:39 +0800

    update Q22 generate parenthesis

--- src/com/leetcode/algorithm/Q22_GenerateParentheses.java
+++ src/com/leetcode/algorithm/Q22_GenerateParentheses.java
@@ -4,18 +4,47 @@ import java.util.*;
 
 public class Q22_GenerateParentheses {
     List<String> results = new ArrayList<String>();
-    List<Character> path = new ArrayList<>(); 
-    int index = 0; //head pointer of path stack
+    String path = ""; 
     
     public List<String> generateParenthesis(int n) {
-        int rp = n; //num of available right parenthesis
-        int lp = n; //letf parenthesis
-        int[] par = {1, -1}; //1 represents '(' and -1 represents')'
-        
-        
+        find(0,n,n);
         return results;
     }
     
-    public void find(int state, int rp, int lp){
+    /**
+     * 
+     * @param state: INIT, state = 0; path.add('(') then state += 1;  path.add(')') then state -=1
+     * @param rp: the No. of available right parenthesis
+     * @param lp: the No. of available right parenthesis
+     * runtime: 2ms
+     */
+    public void find(int state,int rp, int lp){
+        //add '('
+        if(rp > 0){
+            rp -= 1;
+            path += '(';
+            state += 1;
+            find(state, rp, lp);
+            state -= 1;
+            path = path.substring(0,path.length()-1);
+            rp += 1;
+        }
+        //add ')'
+        if(state > 0){
+            lp -= 1;
+            path += ')';
+            state -= 1;
+            
+            //reach the end of the tree
+            if(lp == 0){
+                String result = path;
+                results.add(result);
+            }
+            
+            find(state, rp, lp);
+            state += 1;
+            path = path.substring(0,path.length()-1);
+            rp -= 1;
+        }
     }
 }
