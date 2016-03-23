b9e8a62299cad991dd288494dd2096c6cab82d52
tianxing <txsing@gmail.com>
2016-03-23 21:34:00 +0800

    add dynamic programming solution for Q62

--- src/com/leetcode/algorithm/Q62_UniquePaths.java
+++ src/com/leetcode/algorithm/Q62_UniquePaths.java
@@ -15,4 +15,23 @@ public class Q62_UniquePaths {
         }
         return (int)result;
     }
+    
+    /***
+     * Dynamic programming
+     * unique(m,n) = unique(m-1, n) + unique(m, n-1)
+     * when m == 0 || n == 0, unique() = 1;
+     */
+    public int uniquePaths2(int m, int n){
+    	int[][] paths = new int[m][n];
+    	for(int i = 0; i<m; i++){
+    		for(int j =0; j<n; j++){
+    			if(i == 0 || j==0){
+    				paths[i][j] = 1;
+    			}else{
+    				paths[i][j] = paths[i-1][j] + paths[i][j-1];
+    			}
+    		}
+    	}
+    	return paths[m-1][n-1];
+    }
 }
