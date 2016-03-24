a01eca71bc6b5a69bfe814dce2837e72f4494860
tianxing <txsing@gmail.com>
2016-03-25 01:07:04 +0800

    update Q20

--- src/com/leetcode/algorithm/Q20_ValidParentheses.java
+++ src/com/leetcode/algorithm/Q20_ValidParentheses.java
@@ -2,23 +2,34 @@ package com.leetcode.algorithm;
 
 public class Q20_ValidParentheses {
     public boolean isValid(String s) {
-        int[] stack = new int[s.length()];
+        char[] stack = new char[s.length()];
+        stack[0] = s.charAt(0);
         int head = 0;
         
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 1; i < s.length(); i++){
             char ch = s.charAt(i);
-            if(match(ch, stack[head])){
+            if(head >=0 && match(stack[head], ch)){
                 head --;
                 continue;
             }
-            stack[head++] = ch;
+            if(ch == ')' || ch == '}' || ch == ']'){
+                return false;
+            }
+            stack[++head] = ch;
         }
         
-        if(head == 0){
+        if(head == -1){
             return true;
         }
         return false;
     }
     
-    bool
+    boolean match(char left, char right){
+        if( (left == '(' && right == ')')
+        ||(left == '{' && right == '}')
+        ||(left == '[' && right == ']')){
+            return true;
+        }
+        return false;
+    }
 }
