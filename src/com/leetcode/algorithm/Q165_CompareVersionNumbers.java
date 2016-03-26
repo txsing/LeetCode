b765f7d57534e4c9fa86e1d92f57fb4d62c0cdc3
tianxing <txsing@gmail.com>
2016-03-26 23:29:39 +0800

    add Q165 Compare Version Numbers

---
+++ src/com/leetcode/algorithm/Q165_CompareVersionNumbers.java
@@ -0,0 +1,21 @@
+package com.leetcode.algorithm;
+
+public class Q165_CompareVersionNumbers {
+	public int compareVersion(String version1, String version2) {
+	    // . is used in regular expression, so here we need add '//''
+	    String[] levels1 = version1.split("\\.");
+	    String[] levels2 = version2.split("\\.");
+
+	    int length = Math.max(levels1.length, levels2.length);
+	    for (int i=0; i<length; i++) {
+	        Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+	        Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+	        int compare = v1.compareTo(v2);
+	        if (compare != 0) {
+	            return compare;
+	        }
+	    }
+
+	    return 0;
+	}
+}
