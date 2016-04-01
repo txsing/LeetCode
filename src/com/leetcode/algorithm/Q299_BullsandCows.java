70977e5e150bdb0e32ed5e38827a9e9e0f5404eb
tianxing <txsing@gmail.com>
2016-04-01 22:38:05 +0800

    update Q299 with detailed comments

--- src/com/leetcode/algorithm/Q299_BullsandCows.java
+++ src/com/leetcode/algorithm/Q299_BullsandCows.java
@@ -1,17 +1,27 @@
 package com.leetcode.algorithm;
 
 public class Q299_BullsandCows {
+	/**
+	 * pick out all matched Bulls
+	 * model Cows match between secret and guess as Supply (+1) & Demand (-1)
+	 * use an array to record the supply & demand state;
+	 * @param secret
+	 * @param guess
+	 * @return
+	 */
 	public String getHint(String secret, String guess) {
 		int bulls = 0;
 		int cows = 0;
 		int[] numbers = new int[10];
 		for (int i = 0; i < secret.length(); i++) {
+			//handle all Bull match
 			if (secret.charAt(i) == guess.charAt(i))
 				bulls++;
+			//handle Cows match as Supply & Demand, numbers array is used as state recording
 			else {
-				if (numbers[secret.charAt(i) - '0']++ < 0)
+				if (numbers[secret.charAt(i) - '0']++ < 0)	//supply array[]++
 					cows++;
-				if (numbers[guess.charAt(i) - '0']-- > 0)
+				if (numbers[guess.charAt(i) - '0']-- > 0)	//demand array[]--
 					cows++;
 			}
 		}
