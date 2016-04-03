4d64c06af08f20c8032a809a616ba742fd0b0f32
tianxing <txsing@gmail.com>
2016-04-03 12:15:45 +0800

    update Q211 maximal square---add DP solution

--- src/com/leetcode/algorithm/Q221_MaximalSquare.java
+++ src/com/leetcode/algorithm/Q221_MaximalSquare.java
@@ -1,49 +1,26 @@
 package com.leetcode.algorithm;
 
 public class Q221_MaximalSquare {
-	public int maximalSquare(char[][] matrix) {
-		int len = matrix.length;
-		int result = 0;
-		int temp = 0;
-		for (int xy = 0; xy < len; xy++) {
-			temp = maxSqrLen(matrix, xy, xy, temp - 1);
-			if (temp == 0) {
-				continue;
-			}
-			int vt = temp;
-			int ht = temp;
-			for (int i = xy + 1; i < len; i++) {
-				vt = maxSqrLen(matrix, xy, i, vt - 1);
-				if (vt > result) {
-					result = vt;
-				}
-
-				ht = maxSqrLen(matrix, i, xy, ht - 1);
-				if (ht > result) {
-					result = ht;
-				}
-			}
-		}
-		return result * result;
+	/**
+	 * DP Algorithm
+	 * build a new array B where B[i,j] is the side length of the maximal 1's square within
+	 * the rectangle (a[0, 0] a[i, j])
+	 * the iterative equation is b[i][j] = Math.min(b[i][j-1], b[i-1][j-1], b[i-1][j]) + 1
+	 * @param a
+	 * @return
+	 */
+	public int maximalSquare(char[][] a) {
+	    if(a.length == 0) return 0;
+	    int m = a.length, n = a[0].length, result = 0;
+	    int[][] b = new int[m+1][n+1];
+	    for (int i = 1 ; i <= m; i++) {
+	        for (int j = 1; j <= n; j++) {
+	            if(a[i-1][j-1] == '1') {
+	                b[i][j] = Math.min(Math.min(b[i][j-1] , b[i-1][j-1]), b[i-1][j]) + 1;
+	                result = Math.max(b[i][j], result); // update result
+	            }
+	        }
+	    }
+	    return result*result;
 	}
-
-	public int maxSqrLen(char[][] matrix, int x, int y, int skips) {
-		if (matrix[x][y] == '0') {
-			return 0;
-		}
-		if (skips < 1) {
-			skips = 1;
-		}
-		while (skips < matrix.length - x && skips < matrix.length - y) {
-			int i = skips;
-			for (int j = 0; j <= i; j++) {
-				if (matrix[x + i][x + j] != '1' || matrix[x + i][y + j] != matrix[x + j][x + i]) {
-					return skips;
-				}
-			}
-			skips++;
-		}
-		return skips;
-	}
-
 }
