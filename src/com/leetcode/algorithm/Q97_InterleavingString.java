b0c078449ad1fc09e7c7d77aa94dc1afd4cc3258
tianxing <txsing@gmail.com>
2016-04-07 22:11:59 +0800

    update Q97 DP Algorithm

--- src/com/leetcode/algorithm/Q97_InterleavingString.java
+++ src/com/leetcode/algorithm/Q97_InterleavingString.java
@@ -1,33 +1,41 @@
 package com.leetcode.algorithm;
 
 public class Q97_InterleavingString {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if(s3.length() != s1.length() + s2.length())
-            return false;
-        return helper(s1,s2,s3,s1.length(),s2.length(),s3.length());
-    }
-    
-    boolean helper(String s1, String s2, String s3,int x, int y, int n){
-        if(x == 0){
-            return s3.substring(0,n-1).contains(s2.substring(0,y-1));
-        }
-        if(y == 0){
-            return s3.substring(0,n-1).contains(s1.substring(0,x-1));
-        }
-        char ch1 = s1.charAt(x-1);
-        char ch2 = s2.charAt(y-1);
-        char ch3 = s3.charAt(n-1);
-        
-        boolean b1 = false;
-        boolean b2 = false;
-        
-        //System.out.println(s1.length()+""+s2.length());
-        if(ch1 == ch3 ){
-            b1 = helper(s1, s2, s3, x-1, y, n-1);
-        }
-        if(ch2 == ch3){
-            b2 = helper(s1, s2, s3, x, y-1, n-1);
-        }
-        return  b1||b2;
-    }
+	/**
+	 * DP Algorithm (Assuming that the index starts from 1 instead of 0)
+	 * F(x, y) = True or False = whether s3[1, x+y] is interleaved by s1[1,x] and s2[1,y]
+	 * Iterative formula: F(x, y) = (F(x-1, y) && s1[x])||(F(x,y-1) && s2[y])
+	 * 
+	 * when x = 0 or y = 0, which means s3[1,x+y] is the substring of s1, s2 respectively.
+	 */
+	public boolean isInterleave(String s1, String s2, String s3) {
+		//wrong input case
+		if (s1.length() + s2.length() != s3.length()) {
+			return false;
+		}
+
+		boolean[][] table = new boolean[s1.length() + 1][s2.length() + 1];
+		
+		//special case when s1=s2=s3=""
+		table[0][0] = true;
+		
+		//Init
+		for (int i = 1; i <= s2.length(); i++) {
+			table[0][i] = (s2.charAt(i - 1) == s3.charAt(i - 1)) && table[0][i - 1];
+		}
+
+		for (int i = 1; i <= s1.length(); i++) {
+			table[i][0] = (s1.charAt(i - 1) == s3.charAt(i - 1)) && table[i - 1][0];
+		}
+		
+		//Iteration
+		for (int i = 1; i <= s1.length(); i++) {
+			for (int j = 1; j <= s2.length(); j++) {
+				table[i][j] = (table[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+						|| (table[i][j - 1]) && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+			}
+		}
+
+		return table[s1.length()][s2.length()];
+	}
 }
