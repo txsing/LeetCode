946a5a1557f007107aca43c50bb85a7831bdd912
tianxing <txsing@gmail.com>
2016-04-12 22:44:59 +0800

    add another non-recursive way to do preorder traversal

--- src/com/leetcode/algorithm/Q144_BinaryTreePreorderTraversal.java
+++ src/com/leetcode/algorithm/Q144_BinaryTreePreorderTraversal.java
@@ -34,4 +34,21 @@ public class Q144_BinaryTreePreorderTraversal {
 		}
 		return result;
 	}
+
+	public List<Integer> preorderTraversal2(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		if (root == null)
+			return result;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.pop();
+			result.add(node.val);
+			if (node.right != null)
+				stack.push(node.right);
+			if (node.left != null)
+				stack.push(node.left);
+		}
+		return result;
+	}
 }
