8bd1901586a56cb50fb955167ac7b9b352cd1aff
tianxing <txsing@gmail.com>
2016-05-09 22:16:18 +0800

    add 33. Search in Rotated Sorted Array

---
+++ src/com/leetcode/algorithm/Q33_SearchinRotatedSortedArray.java
@@ -0,0 +1,56 @@
+package com.leetcode.algorithm;
+
+public class Q33_SearchinRotatedSortedArray {
+	public int search(int[] nums, int target) {
+		int len = nums.length;
+		int l = 0, r = len - 1;
+		int mid = 0;
+		if (nums.length == 0)
+			return -1;
+		if (nums.length == 1) {
+			return nums[0] == target ? 0 : -1;
+		}
+		//find the minimal value
+		while (l < r) {
+			mid = (l + r) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+
+			if (nums[mid] > nums[r])
+				l = mid + 1;
+			else
+				r = mid;
+		}
+		mid = r;
+		int index = bs(nums, 0, mid - 1, target);
+		return index != -1 ? index : bs(nums, mid, len - 1, target);
+	}
+
+	public static int bs(int[] nums, int s, int e, int target) {
+		int mid;
+
+		if (s < 0 || e < 0 || e > nums.length)
+			return -1;
+
+		if (s == e) {
+			if (target == nums[s])
+				return s;
+			else
+				return -1;
+		}
+
+		while (s <= e) {
+			//noted that it is "s + (e - s)/2"
+			mid = s + (e - s) / 2;
+			if (nums[mid] == target)
+				return mid;
+			if (nums[mid] > target)
+				e = mid - 1;
+			else
+				s = mid + 1;
+		}
+
+		return -1;
+	}
+}
