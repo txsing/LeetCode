c3a7cbf6d9e97df2cc0863a066f095cee06c6d7a
tianxing <txsing@gmail.com>
2016-05-24 00:32:50 +0800

    improve 54. Spiral Matrix

--- src/com/leetcode/algorithm/Q54_SpiralMatrix.java
+++ src/com/leetcode/algorithm/Q54_SpiralMatrix.java
@@ -3,32 +3,45 @@ package com.leetcode.algorithm;
 import java.util.*;
 
 public class Q54_SpiralMatrix {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<Integer>();
-        
-        int n = matrix.length;
-        for(int i = 0; i < (n+1)/2; i++){
-            spiralSquare(matrix, res, i, n - i*2);
-        }
-        
-        return res;
-    }
-    
-    public void spiralSquare(int[][] M, List<Integer> lst, int index ,int n){
-        if(n == 1){
-            lst.add(M[index][index]);
-            return;
-        }
-        for(int i = 0; i < n - 1; i++)
-            lst.add(M[index][index+i]);
-        
-        for(int i = 0; i < n - 1; i++)
-            lst.add(M[index+i][index+n-1]);
-        
-        for(int i = 0; i < n - 1; i++)
-            lst.add(M[index+n-1][index+n-1-i]);
-        
-        for(int i = 0; i < n - 1; i++)
-            lst.add(M[index+n-1-i][index]);
-    }
+	public List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> res = new ArrayList<Integer>();
+		int m = matrix.length;
+
+		if (m == 0)
+			return res;
+
+		int n = matrix[0].length;
+
+		for (int i = 0; i < (Math.min(m, n) + 1) / 2; i++) {
+			spiral(matrix, res, i, n - i * 2, m - i * 2);
+		}
+
+		return res;
+	}
+
+	public void spiral(int[][] M, List<Integer> lst, int index, int n, int m) {
+		if (m == 1) {
+			for (int i = 0; i < n; i++)
+				lst.add(M[index][index + i]);
+			return;
+		}
+
+		if (n == 1) {
+			for (int i = 0; i < m; i++)
+				lst.add(M[index + i][index]);
+			return;
+		}
+
+		for (int i = 0; i < n - 1; i++)
+			lst.add(M[index][index + i]);
+
+		for (int i = 0; i < m - 1; i++)
+			lst.add(M[index + i][index + n - 1]);
+
+		for (int i = 0; i < n - 1; i++)
+			lst.add(M[index + m - 1][index + n - 1 - i]);
+
+		for (int i = 0; i < m - 1; i++)
+			lst.add(M[index + m - 1 - i][index]);
+	}
 }
